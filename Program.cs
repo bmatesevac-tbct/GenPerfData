6bc5c69e329c4e416860b36cb0b10e48e96e1897
Bob Matesevac <bob.matesevac@terumobct.com>
2021-02-10 10:34:07 -0500

    Now building facilities and devices

--- Program.cs
+++ Program.cs
@@ -12,9 +12,109 @@ using _Excel = Microsoft.Office.Interop.Excel;
 namespace GenPerfData
 {
 
+   class Facility
+   {
+      public string Id;
+      public string Name;
+      public string City;
+      public string State;
+      public string ZoneId;
+      public bool Active;
+   }
+
+   class FacilitySchedule
+   {
+
+   }
+
+   class Device
+   {
+      public string Name;
+      public string SerialNumber;
+      public string FacilityId;
+      public bool Active;
+   }
+
+   class DevicesWorksheet
+   {
+      private const int DeviceNameCell = 1;
+      private const int SerialCell = 2;
+      private const int FacilityIdCell = 3;
+      private const int DeviceStatusCell = 4;
+
+      private Worksheet _worksheet;
+      public DevicesWorksheet(Worksheet worksheet)
+      {
+         _worksheet = worksheet;
+         _worksheet.Name = "Devices";
+         _worksheet.Cells[1, DeviceNameCell] = "Device name";
+         _worksheet.Cells[1, SerialCell] = "Serial number";
+         _worksheet.Cells[1, FacilityIdCell] = "Facility ID";
+         _worksheet.Cells[1, DeviceStatusCell] = "Device Active=1/Inactive=0";
+      }
+
+      public void AddDevices(List<Device> devices)
+      {
+         int row = 2;
+         var cells = _worksheet.Cells;
+         foreach (var device in devices)
+         {
+            cells[row, DeviceNameCell] = device.Name;
+            cells[row, SerialCell] = device.SerialNumber;
+            cells[row, FacilityIdCell] = device.FacilityId;
+            cells[row, DeviceStatusCell] = device.Active ? "1" : "0";
+            ++row;
+         }
+      }
+
+   }
+
+   class FacilitiesWorkSheet
+   {
+      private const int IdCell = 1;
+      private const int NameCell = 2;
+      private const int CityCell = 3;
+      private const int StateCell = 4;
+      private const int ZoneCell = 5;
+      private const int ActiveCell = 6;
+
+      private Worksheet _worksheet;
+
+      public FacilitiesWorkSheet(Worksheet worksheet)
+      {
+         _worksheet = worksheet;
+         _worksheet.Name = "Facilities";
+         _worksheet.Cells[1, IdCell] = "Facility ID";
+         _worksheet.Cells[1, NameCell] = "Facility name";
+         _worksheet.Cells[1, CityCell] = "Facility city";
+         _worksheet.Cells[1, StateCell] = "Facility state";
+         _worksheet.Cells[1, ZoneCell] = "IANA zone ID";
+         _worksheet.Cells[1, ActiveCell] = "Facility Active=1/Inactive=0";
+      }
+
+      public void AddFacilities(List<Facility> facilities)
+      {
+         int row = 2;
+         var cells = _worksheet.Cells;
+         foreach (var facility in facilities)
+         {
+
+            cells[row, IdCell] = facility.Id;
+            cells[row, NameCell] = facility.Name;
+            cells[row, CityCell] = facility.City;
+            cells[row, StateCell] = facility.State;
+            cells[row, ZoneCell] = facility.ZoneId;
+            cells[row, ActiveCell] = facility.Active ? "1" : "0";
+            ++row;
+         }
+      }
+   }
+
 
    class Program
    {
+
+
       static void Main(string[] args)
       {
          var program = new Program();
@@ -22,36 +122,72 @@ namespace GenPerfData
       }
 
 
-
+      private int _numDevices = 2 ;
+      private int _numFacilities = 2;
 
       _Application _excel = new _Excel.Application();
       Workbook _workbook;
-      Worksheet _worksheet;
+      private FacilitiesWorkSheet _facilitiesWorksheet;
+      private Worksheet _facilitiesSchedultWorksheet;
+      private DevicesWorksheet _devicesWorksheet;
 
-      public void NewFile()
-      {
-         _workbook = _excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
-         _worksheet = _workbook.Worksheets[1];
-         _worksheet.Name = "Worksheet0";
-      }
+      private List<Facility> _facilities = new List<Facility>();
+      private List<Device> _devices = new List<Device>();
 
-      // Method adds a new worksheet to the existing workbook 
-      public Worksheet NewSheet()
+
+      public void CreateData()
       {
-         return _excel.Worksheets.Add(After: _worksheet);
+
+         for (int nFacility = 1; nFacility <= _numFacilities; ++nFacility)
+         {
+            var facility = new Facility()
+            {
+               Id = String.Format("Site{0:000}", nFacility),
+               Name = String.Format("Facility{0:000}", nFacility),
+               City = "ACity",
+               State = "AState",
+               ZoneId = "US/Mountain",
+               Active = true
+            };
+            _facilities.Add(facility);
+         }
+
+         // create devices
+         for (int nDevice = 1; nDevice <= _numDevices; ++nDevice)
+         {
+            var device = new Device()
+            {
+               Name = String.Format("Device{0:000}", nDevice),
+               SerialNumber = String.Format("1X{0:00000}", nDevice),
+               FacilityId = "Denver",
+               Active = true,
+            };
+            _devices.Add(device);
+         }
       }
 
 
       public void Run(string[] args)
       {
-         // Create a new workbook with a single sheet
-         NewFile();
+         CreateData();
 
-         // Add a new sheet to the workbook
-         var sheet = NewSheet();
-         sheet.Name = "MyWorksheet";
-         sheet = NewSheet();
-         sheet.Name = "MyWorksheetAgain";
+         _workbook = _excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+
+         // create worksheets
+         var worksheet = _workbook.Worksheets[1];
+         _facilitiesWorksheet = new FacilitiesWorkSheet(worksheet);
+
+
+         _facilitiesSchedultWorksheet = _excel.Worksheets.Add(After: worksheet);
+         _facilitiesSchedultWorksheet.Name = "Facilities schedule";
+
+
+         worksheet = _excel.Worksheets.Add(After: _facilitiesSchedultWorksheet);
+         _devicesWorksheet = new DevicesWorksheet(worksheet);
+
+
+         _devicesWorksheet.AddDevices(_devices);
+         _facilitiesWorksheet.AddFacilities(_facilities);
 
 
 
