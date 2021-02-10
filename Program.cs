53a94ebd2472bbbc63de768a37fc38a897f5998e
Bob Matesevac <bob.matesevac@terumobct.com>
2021-02-10 11:18:29 -0500

    Add support for facility schedule

--- Program.cs
+++ Program.cs
@@ -20,11 +20,19 @@ namespace GenPerfData
       public string State;
       public string ZoneId;
       public bool Active;
+      public FacilitySchedule Schedule;
    }
 
-   class FacilitySchedule
+   class FacilityScheduledEvent
    {
+      public string FacilityId;
+      public int DayOfWeek;
+      public string DataTransmissionStart;
+      public int DurationHours;
+   }
 
+   class FacilitySchedule : List<FacilityScheduledEvent> 
+   {
    }
 
    class Device
@@ -110,6 +118,42 @@ namespace GenPerfData
       }
    }
 
+   class FacilitiesScheduleWorksheet
+   {
+      private const int IdCell = 1;
+      private const int DayOfWeekCell = 2;
+      private const int TxStartCell = 3;
+      private const int DurationCell = 4;
+
+
+      private Worksheet _worksheet;
+      private int _insertRow = 2;
+
+      public FacilitiesScheduleWorksheet(Worksheet worksheet)
+      {
+         _worksheet = worksheet;
+         _worksheet.Name = "Facilities schedule";
+
+         _worksheet.Cells[1, IdCell] = "Facility ID";
+         _worksheet.Cells[1, DayOfWeekCell] = "Day of week";
+         _worksheet.Cells[1, TxStartCell] = "Data transmission start (24 hour clock)";
+         _worksheet.Cells[1, DurationCell] = "Duration (Hours)";
+      }
+
+      public void AddSchedule(FacilitySchedule schedule)
+      {
+         foreach (var evnt in schedule)
+         {
+            _worksheet.Cells[_insertRow, IdCell] = evnt.FacilityId;
+            _worksheet.Cells[_insertRow, DayOfWeekCell] = evnt.DayOfWeek.ToString();
+            _worksheet.Cells[_insertRow, TxStartCell] = evnt.DataTransmissionStart;
+            _worksheet.Cells[_insertRow, DurationCell] = evnt.DurationHours.ToString();
+            ++_insertRow;
+         }
+
+      }
+   }
+
 
    class Program
    {
@@ -128,7 +172,7 @@ namespace GenPerfData
       _Application _excel = new _Excel.Application();
       Workbook _workbook;
       private FacilitiesWorkSheet _facilitiesWorksheet;
-      private Worksheet _facilitiesSchedultWorksheet;
+      private FacilitiesScheduleWorksheet _facilitiesScheduleWorksheet;
       private DevicesWorksheet _devicesWorksheet;
 
       private List<Facility> _facilities = new List<Facility>();
@@ -150,6 +194,24 @@ namespace GenPerfData
                Active = true
             };
             _facilities.Add(facility);
+
+
+            // create scheduled events
+            var schedule = new FacilitySchedule();
+            facility.Schedule = schedule;
+
+            for (int day = 1; day <= 7; ++day)
+            {
+               var scheduledEvent = new FacilityScheduledEvent()
+               {
+
+                  FacilityId = facility.Id,
+                  DayOfWeek = day,
+                  DataTransmissionStart = "00:00",
+                  DurationHours = 1
+               };
+               schedule.Add(scheduledEvent);
+            }
          }
 
          // create devices
@@ -177,19 +239,20 @@ namespace GenPerfData
          var worksheet = _workbook.Worksheets[1];
          _facilitiesWorksheet = new FacilitiesWorkSheet(worksheet);
 
+         worksheet = _excel.Worksheets.Add(After: worksheet);
+         _facilitiesScheduleWorksheet = new FacilitiesScheduleWorksheet(worksheet);
 
-         _facilitiesSchedultWorksheet = _excel.Worksheets.Add(After: worksheet);
-         _facilitiesSchedultWorksheet.Name = "Facilities schedule";
-
-
-         worksheet = _excel.Worksheets.Add(After: _facilitiesSchedultWorksheet);
+         worksheet = _excel.Worksheets.Add(After: worksheet);
          _devicesWorksheet = new DevicesWorksheet(worksheet);
 
 
          _devicesWorksheet.AddDevices(_devices);
          _facilitiesWorksheet.AddFacilities(_facilities);
 
-
+         foreach (var facility in _facilities)
+         {
+            _facilitiesScheduleWorksheet.AddSchedule(facility.Schedule);
+         }
 
          var path = Directory.GetCurrentDirectory();
          var filePath = $"{path}/MyTestData.xlsx";
